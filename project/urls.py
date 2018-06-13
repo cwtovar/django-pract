91a6c09a33894a015f47f986f2dbe8b6705bf7c9
Chris <cwtovar@miners.utep.edu>
2018-06-13 13:04:52 -0600

    edit views and urls

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-#    url(r'^landpage/',include('landpage.urls')),
+    # url(r'^landpage/',include('landpage.urls')),
     url(r'^$', index),
     url(r'^$', view1),
     url(r'^health$', health),
--- welcome/urls.py
+++ welcome/urls.py
@@ -1,4 +1,4 @@
 from django.conf.urls import url
 from . import views
 
-urlpatters = [url(r'^$', views.view1, name='view1'),]
+urlpatterns = [url(r'^$', views.view1, name='view1'),]
