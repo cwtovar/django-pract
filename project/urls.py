e1a507b392c6d1131cffbaa7e85c54dcc8445f96
Chris <cwtovar@miners.utep.edu>
2018-06-14 07:33:01 -0600

    Point-to-views

--- project/urls.py
+++ project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
    
-    url(r'^$', index),
+   # url(r'^$', index),
     url(r'^$', include('welcome.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
