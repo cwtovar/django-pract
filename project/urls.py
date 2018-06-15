112ef21ed4a8eaaa116ec37ea1318cae3ceb82f6
Chris <cwtovar@miners.utep.edu>
2018-06-15 08:28:56 -0600

    Ref Check

--- project/urls.py
+++ project/urls.py
@@ -2,15 +2,15 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, view1
+from welcome.views import index
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
    
-    url(r'^$', index),
-    url(r'^view1', view1),
+    url(r'^$', index, name='index'),
+    url(r'^pages/', include('welcome.urls')),    
     url(r'^admin/', include(admin.site.urls)),
 ]
 
