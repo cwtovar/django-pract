305a463729024234c755429ae4f1ced36852d1b4
Chris <cwtovar@miners.utep.edu>
2018-06-15 09:28:47 -0600

    not as brouken

--- project/urls.py
+++ project/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from welcome.views import index
+from welcome.views import index, view1
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
    
-   # url(r'^$', index, name='index'),
+    url(r'^view1', view1, name='view1'),
     url(r'^', include('personal.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^pages/', include('welcome.urls')),    
