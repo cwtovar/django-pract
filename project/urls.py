22f904224617fac91f024d99793889c75d6e5fb2
Chris <cwtovar@miners.utep.edu>
2018-06-15 08:52:33 -0600

    editted project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-#from django.conf import settings
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -14,10 +14,6 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^pages/', include('welcome.urls')),    
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+
